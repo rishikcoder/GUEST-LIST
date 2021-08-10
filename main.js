443718f22239e6883ea1281b804601b336a1c3f0
rishikcoder <84475398+rishikcoder@users.noreply.github.com>
2021-08-10 14:05:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+var names_of_people = [];
+    
+function submit()
+{
+    var GuestName = document.getElementById("name1").value;
+	// use the push function as - names_of_people.push(GuestName);
+	console.log(GuestName);    
+    console.log(names_of_people);
+    var lenght_of_name = names_of_people.length;
+    console.log(lenght_of_name);
+	document.getElementById("display_name").innerHTML=names_of_people.toString();
+   }
+
+
+
+   function submit() {
+    var display_student_array = [];
+
+    for (var j = 1; j <= 4; j++) {
+        var name_of_the_student = document.getElementById("name_of_the_student_" + j).value
+        console.log(name_of_the_student);
+        name_of_the_student_array.push(name_of_the_student);
+    }
+    console.log(name_of_the_student_array);
+
+    var length_of_name_student_array = name_of_the_student_array.length;
+    console.log(length_of_name_student_array);
+
+
+	function sorting()
+	{
+		names_of_people.sort();
+		var i= names_of_people.join("<br>");
+		console.log(names_of_people);		
+		document.getElementById("sorted").innerHTML=i.toString();
+		}
+	
+	function show()
+	{
+		var i= names_of_people.join("<br>");
+		console.log(names_of_people);
+		document.getElementById("p1").innerHTML=i.toString();
+		document.getElementById("sort_button").style.display="block";
+		}
+	
+	
+	
+function searching()
+{
+	var s= document.getElementById("s1").value;
+	var found=0;
+	var j;
+	for(j=0; j<names_of_people.length; j++)
+		{
+			if(s==names_of_people[j]){
+				found=found+1;
+			}	
+		}
+	document.getElementById("p2").innerHTML="name found "+found+" time/s";
+	console.log("found name "+found+" time/s");
+}
